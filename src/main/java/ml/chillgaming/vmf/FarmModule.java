1e9b10fdb66968e29d7f199c102b44356abade8b
Liz <elizabethlfransen@gmail.com>
2020-11-25 11:19:56 -0600

    Worked on base command and configuration

---
+++ src/main/java/ml/chillgaming/vmf/FarmModule.java
@@ -0,0 +1,10 @@
+package ml.chillgaming.vmf;
+
+public interface FarmModule {
+    /**
+     * Gets the upfront cost of a module.
+     * @return The cost of the module.
+     */
+    double getCost();
+    double getUpkeep();
+}
---
+++ src/main/java/ml/chillgaming/vmf/command/VMFBaseCommand.java
@@ -0,0 +1,22 @@
+package ml.chillgaming.vmf.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.List;
+
+public class VMFBaseCommand implements CommandExecutor, TabCompleter {
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return null;
+    }
+}
